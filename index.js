84bdca53b99b095c69c3a392ac853da54ed739fb
Thomas Hunter <thomashunter@Thomass-MacBook-Pro-3.local>
2024-03-04 11:43:40 -0800

    first push of secure the vault

---
+++ index.js
@@ -0,0 +1,12 @@
+//create a variable for the first number with an equation that equals 10.
+const numOne = 5 * 2;
+
+//creatue a variable for the second number with an equation that equals 40.
+const numTwo = 11 * 4 - 4;
+
+//create a variable for the third number with an equation that equals 39.
+const numThree = 80 / 2 - 1;
+
+//alert the message and vault combination so they show up in a dialoge box.
+
+alert(`You have recieved this message because you have been chosen to open an important vault. Here is the combination: ${numOne}-${numTwo}-${numThree}`)
